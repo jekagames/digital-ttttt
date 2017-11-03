22fc6ead7d8f6410fbcc50b040e98bb34c1592e8
Jess Marcotte <meaikoh@gmail.com>
2017-11-03 18:22:51 -0400

    Audio Working with Lizard!
    -- When lizard is clicked, the click is detected
    -- Audio plays from an array of lizard audio clips
    -- No new audio while current clip is playing
    
    Next needs to be extended to other objects

---
+++ Assets/Scripts/lizard_OnClickPlaySound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lizard_OnClickPlaySound : MonoBehaviour {
+
+	public List<AudioClip> lizardClips = new List<AudioClip>();
+
+	void Start(){
+		// SoundFolder should be in Assets/Resources 
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-01"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-02"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-03"));
+		lizardClips.Add ((AudioClip)Resources.Load ("ounds/lizard-04"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-05"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-06"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-07"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-08"));
+		lizardClips.Add ((AudioClip)Resources.Load ("Sounds/lizard-09"));
+		Debug.Log("Did the Audio load?");
+	}
+
+		void OnMouseDown ()
+		{
+			Debug.Log("Clicked the lizard!");
+		if (!transform.GetComponent<AudioSource>().isPlaying)
+		{
+		transform.GetComponent<AudioSource>().clip =  lizardClips[Random.Range(0,lizardClips.Count)];
+		transform.GetComponent<AudioSource>().Play();
+		}
+	}
+}
+
+
