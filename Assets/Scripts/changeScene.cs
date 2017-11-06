5eda43bf391c8af57514f845e6fee77c90ea7881
Jess Marcotte <meaikoh@gmail.com>
2017-11-06 18:11:13 -0500

    Playing with text overlays and scene changes

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	// Update is called once per frame
+	void sceneChange () {
+		if (Input.GetKey(KeyCode.UpArrow))
+			Debug.Log ("A key or mousepress was detected");
+		SceneManager.LoadScene("ttttt-main", LoadSceneMode.Single);
+	}
+}
