42513d85a0c6c003edaf24040c6e888f8a381517
Jess Marcotte <meaikoh@gmail.com>
2017-11-08 20:24:13 -0500

    Prompt Generation Implemented
    Need to create the list, but the prompt generation works and appears in
    the appropriate place in Fungus. Loop also created for repeated
    generation.

---
+++ Assets/Scripts/generatePrompt.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class generatePrompt : MonoBehaviour {
+
+	public Flowchart maingame;
+	public string myPrompt1;
+	public string myPrompt2;
+
+	// Use this for initialization
+	void Start () {
+
+			getPrompt ();
+			maingame.SetStringVariable ("myPrompt1", myPrompt1);
+			maingame.SetStringVariable ("myPrompt2", myPrompt2);
+		}
+
+	void getPrompt(){
+		Debug.Log ("Getting the prompt");
+		List<string> promptGen = new List<string>();
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+		promptGen.Add("time management");
+
+		myPrompt1 = promptGen [Random.Range (0, Mathf.RoundToInt(promptGen.Count - 1))];
+		myPrompt2 = promptGen [Random.Range (0, Mathf.RoundToInt(promptGen.Count - 1))];
+		Debug.Log ("Got the prompt");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
