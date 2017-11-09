ac3209526fbc2d86d101ed1ed26de6ae0dda819b
Jess Marcotte <meaikoh@gmail.com>
2017-11-08 23:08:31 -0500

    Manual and Escape Key working
    Both clicking the manual and pressing Escape open up the menu.
    
    Need to disable audio-playing scripts when Fungus menus are open.  What
    a pain.

--- Assets/Scripts/onManualClick.cs
+++ Assets/Scripts/onManualClick.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
 
 public class onManualClick : MonoBehaviour {
-
+	public Flowchart flowchart;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnMouseDown () {
+		manualButtonClicked ();
+	}
+
+	void manualButtonClicked() {
+			Debug.Log ("You clicked the Manual");
+			flowchart.ExecuteBlock ("Menu");
 	}
 }
---
+++ Assets/Scripts/pauseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class pauseManager : MonoBehaviour {
+
+	public Flowchart flowchart;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update(){
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+			{
+			Debug.Log ("You pressed Escape");	
+				flowchart.ExecuteBlock("Menu");
+		}
+
+				}
+
+
+}
