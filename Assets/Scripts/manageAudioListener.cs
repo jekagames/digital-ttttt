3875bd5d47268b39692706d0dae1bf93113c9f37
Jess Marcotte <meaikoh@gmail.com>
2017-11-09 15:25:59 -0500

    generated first build, many things broke
    Managed to get the AudioListener to be disabled from a fungus script
    call, but this doesn't work in the build despite working in the editor.
    
    BUGS to fix:
    -- text size is tiny at high resolutions/doesn't scale relative to
    chosen resolution
    -- audio listener not correctly enabling/disabling
    -- Menu options not choosing correct dialog formatting from the Fungus
    options.
    -- Positive and negative characteristic lists continously choosing the
    same option for some reason. Will investigate.

---
+++ Assets/Scripts/manageAudioListener.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+
+public class manageAudioListener : MonoBehaviour
+{
+	public Flowchart SeparatedInstructions;
+	public Flowchart PauseMenu;
+	public Flowchart EndofGameText;
+	public Flowchart MainGameText;
+	private AudioListener myAudioListener;
+
+	void Start ()
+	{
+
+		myAudioListener = GetComponent<AudioListener>();
+		Debug.Log ("I got the AudioListener");
+	}
+
+	void Update(){
+		//Debug.Log("Hello I am trying to call these methods");
+		//disableListener ();
+		//enableListener ();
+	}
+	
+		void disableListener ()
+	{
+		//if (Input.GetKeyDown (KeyCode.A)) {
+			Debug.Log ("Hello I am trying to work on disabling");
+			myAudioListener.enabled = false;
+			Debug.Log ("Listener Disabled");
+		//}
+	}
+		
+	void enableListener ()
+	{
+		//if (Input.GetKeyDown (KeyCode.S)) {
+			Debug.Log ("Hello I am trying to work on enabling");
+			myAudioListener.enabled = true;
+			Debug.Log ("Listener Enabled");
+		//}
+		
+	}
+}
